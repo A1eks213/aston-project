8928964805a3c0a70ae41a347534aa7dc62ed9a4
Алексей Подобашев <aleks.podobashev2002@mail.ru>
2023-12-20 13:04:14 +0300

    fix history #2 (delete context)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,8 @@ import React, { useContext } from 'react';
 import { Card } from '../../components/Card';
 import { useGetPlayersQuery } from '../../RTKQuery/playersApi';
 import { Loader } from '../../components/Loader';
-import { IsSearchBarVisible } from '../../context/SearchbarContext';
 export default function HomePage() {
   const { data: players, isLoading } = useGetPlayersQuery();
-  const { setIsSearchBarVisible } = useContext(IsSearchBarVisible);
-  setIsSearchBarVisible(true)
   if (isLoading) return <Loader />
   return (
     <div className='container'>
